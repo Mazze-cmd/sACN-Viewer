03cb9006deaa436d6453c92c5bef4a6e582e0edd
Mazze-cmd <mg.aderhold@gmail.com>
2026-08-28 16:57:30 +0200

    Delete PyApp_v04 directory

--- PyApp_v04/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Builds Signal.exe from app.py using PyInstaller.
-REM Run this on Windows, inside this folder, after installing requirements.txt.
-
-pyinstaller --noconfirm --onefile --windowed --name Signal app.py
-
-echo.
-echo Done. Find Signal.exe in the "dist" folder.
-pause
